4bf14c31f5f45333b29d9114d56615005262e164
Nirwansyah Ramdhani <nirwansyah.r@kiranatama.com>
2023-03-31 16:15:09 +0700

    profile page post feed

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -10,6 +10,32 @@ import { z } from "zod";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import { filterUserForClient } from "../helpers/filterUserForClient";
+import type { Post } from "@prisma/client";
+
+const addUserDataToPost = async (posts: Post[]) => {
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100
+    })
+  ).map(filterUserForClient);
+
+  return posts.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
+    if (!author || !author.username)
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author not found"
+      });
+    return {
+      post,
+      author: {
+        ...author,
+        username: author.username
+      }
+    };
+  });
+};
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
@@ -24,29 +50,22 @@ export const postsRouter = createTRPCRouter({
       take: 100,
       orderBy: [{ createdAt: "desc" }]
     });
-    const users = (
-      await clerkClient.users.getUserList({
-        userId: posts.map((post) => post.authorId),
-        limit: 100
-      })
-    ).map(filterUserForClient);
-
-    return posts.map((post) => {
-      const author = users.find((user) => user.id === post.authorId);
-      if (!author || !author.username)
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Author not found"
-        });
-      return {
-        post,
-        author: {
-          ...author,
-          username: author.username
-        }
-      };
-    });
+    return addUserDataToPost(posts);
   }),
+
+  getPostsByUserId: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(({ ctx, input }) =>
+      ctx.prisma.post
+        .findMany({
+          where: {
+            authorId: input.userId
+          },
+          take: 100,
+          orderBy: [{ createdAt: "desc" }]
+        })
+        .then(addUserDataToPost)
+    ),
 
   create: privateProcedure
     .input(z.object({ content: z.string().emoji().min(1).max(10) }))
