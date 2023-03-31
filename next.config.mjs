9c09de82186077d53baf466e6e5e626ba322d878
Nirwansyah Ramdhani <nirwansyah.r@kiranatama.com>
2023-03-31 11:38:46 +0700

    Next image, update styles

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    domains: ["images.clerk.dev"]
+  },
 
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
@@ -16,7 +19,7 @@ const config = {
    */
   i18n: {
     locales: ["en"],
-    defaultLocale: "en",
-  },
+    defaultLocale: "en"
+  }
 };
 export default config;
